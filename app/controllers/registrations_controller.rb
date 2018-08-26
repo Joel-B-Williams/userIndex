1cf8e907a81c87e915f170dae735667a17408052
Joel Williams <joel.b.williams.is@gmail.com>
2018-08-26 12:56:38 -0500

    user authentication for edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :color)
     end
-    # def account_update_params
-    #   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-    # end
+    
+    def account_update_params
+      params.require(:user).permit(:color, :first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
 end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,9 @@
+class UserPolicy < ApplicationPolicy
+  def edit?
+    user == record
+  end
+
+  def update?
+    edit?
+  end
+end
